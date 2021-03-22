04f227fd8d88fdd2abf60aba5195c393e377c04e
Zhylkaaa <zhylko.dima@gmail.com>
2021-03-22 09:30:11 +0100

    * add test endpoints * moved config to application context * add basic security

--- src/main/java/pl/edu/agh/ki/io/Application.java
+++ src/main/java/pl/edu/agh/ki/io/Application.java
@@ -2,7 +2,11 @@ package pl.edu.agh.ki.io;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class Application {
 
@@ -10,4 +14,18 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("secret")
+    public String secret() {
+        return "secret";
+    }
+
+    /*@GetMapping("login")
+    public String login() {
+        return "login";
+    }*/
 }

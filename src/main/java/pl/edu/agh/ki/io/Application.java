9dbab01f364da7e246b79ed44c87d3938832d029
AndrzejPodo <andrzejpodobinski2@gmail.com>
2021-03-25 22:28:53 +0100

    add google cloud storage service

--- src/main/java/pl/edu/agh/ki/io/Application.java
+++ src/main/java/pl/edu/agh/ki/io/Application.java
@@ -3,9 +3,9 @@ package pl.edu.agh.ki.io;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "pl.edu.agh.ki.io" })
 public class Application {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/pl/edu/agh/ki/io/cloudstorage/GoogleCloudFileService.java
+++ src/main/java/pl/edu/agh/ki/io/cloudstorage/GoogleCloudFileService.java
@@ -5,8 +5,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
@@ -16,7 +19,7 @@ import com.google.cloud.storage.Storage.PredefinedAcl;
 @Component
 public class GoogleCloudFileService {
 
-    public String upload(MultipartFile file) throws IOException {
+    public void upload(MultipartFile file) throws IOException {
         try {
             StorageOptions storageOptions = StorageOptions.newBuilder().setProjectId("sprainedankle")
                     .setCredentials(GoogleCredentials
@@ -24,12 +27,25 @@ public class GoogleCloudFileService {
                     .build();
             Storage storage = storageOptions.getService();
             String name = file.getName();
-            BlobInfo blobInfo = storage.create(BlobInfo.newBuilder("sprained-ankle-photos", name).build(),
-                    file.getBytes(), BlobTargetOption.predefinedAcl(PredefinedAcl.PUBLIC_READ));
-
-            return blobInfo.getMediaLink();
+            storage.create(BlobInfo.newBuilder("sprained-ankle-photos", name).build(),
+                    file.getBytes(), BlobTargetOption.predefinedAcl(PredefinedAcl.PRIVATE));
         } catch (IllegalStateException e) {
             throw new RuntimeException();
+        }
+    }
+
+    public void download(String photoName, String destFilePath) {
+        try {
+            StorageOptions storageOptions = StorageOptions.newBuilder().setProjectId("sprainedankle")
+                    .setCredentials(GoogleCredentials
+                            .fromStream(new FileInputStream("gcloud-credentials/sprainedankle-4c6c48239f8b.json")))
+                    .build();
+            Storage storage = storageOptions.getService();
+            Blob blob = storage.get(BlobId.of("sprained-ankle-photos", photoName));
+
+            blob.downloadTo(Paths.get(destFilePath));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }

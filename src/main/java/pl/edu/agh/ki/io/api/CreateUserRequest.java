b4bc7bbd092c325d53b33ef0664e032843447f2f
Dima <zhylko.dima@gmail.com>
2021-05-10 15:18:44 +0200

    [SA-128] basic password validation

--- src/main/java/pl/edu/agh/ki/io/api/CreateUserRequest.java
+++ src/main/java/pl/edu/agh/ki/io/api/CreateUserRequest.java
@@ -7,6 +7,7 @@ import pl.edu.agh.ki.io.models.User;
 import pl.edu.agh.ki.io.validation.PasswordMatches;
 import pl.edu.agh.ki.io.validation.SimpleSqlProtected;
 import pl.edu.agh.ki.io.validation.ValidEmail;
+import pl.edu.agh.ki.io.validation.ValidPassword;
 
 import javax.validation.constraints.NotNull;
 import java.sql.Date;
@@ -22,6 +23,7 @@ public class CreateUserRequest {
 
     @NotNull
     @SimpleSqlProtected
+    @ValidPassword
     private String password;
     private String matchingPassword;
 

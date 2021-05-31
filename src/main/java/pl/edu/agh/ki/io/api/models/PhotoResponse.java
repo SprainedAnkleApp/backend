c271592706f8acc56d15d8eea119a76993b74ec9
piotrpolec <piotr.jan.polec@gmail.com>
2021-05-31 22:09:41 +0200

    Add ids to peakResponse and PhotoResponse

--- src/main/java/pl/edu/agh/ki/io/api/models/PhotoResponse.java
+++ src/main/java/pl/edu/agh/ki/io/api/models/PhotoResponse.java
@@ -14,12 +14,14 @@ import java.util.stream.Collectors;
 @Data
 @Builder
 public class PhotoResponse extends WallItemResponse{
+    private Long id;
     private String content;
     private String signedUrl;
     private List<ReactionResponse> reactions;
 
     public static PhotoResponse fromPhotoAndReactions(Photo photo, List<Reaction> reactions) {
         return PhotoResponse.builder()
+                .id(photo.getId())
                 .content(photo.getContent())
                 .signedUrl(photo.getPhotoPath())
                 .reactions(reactions.stream().map(ReactionResponse::fromReaction).collect(Collectors.toList())).build();

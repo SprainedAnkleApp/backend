a88bdf1b52b0bb86ae4db495e9bd68b8626dea45
Dima <zhylko.dima@gmail.com>
2021-06-02 06:42:15 +0200

    add achievements to user/me

--- src/main/java/pl/edu/agh/ki/io/api/models/UserResponse.java
+++ src/main/java/pl/edu/agh/ki/io/api/models/UserResponse.java
@@ -2,9 +2,11 @@ package pl.edu.agh.ki.io.api.models;
 
 import lombok.Builder;
 import lombok.Data;
+import pl.edu.agh.ki.io.api.providers.AchievementsProvider;
 import pl.edu.agh.ki.io.models.User;
 
 import java.sql.Date;
+import java.util.List;
 
 @Data
 @Builder
@@ -18,6 +20,7 @@ public class UserResponse {
     private Date birthday;
     private String about;
     private String phoneNumber;
+    private List<AchievementsProvider.Achievement> achievements;
 
 
     public static UserResponse fromUser(User user) {
@@ -31,6 +34,21 @@ public class UserResponse {
                 .birthday(user.getBirthday())
                 .about(user.getAbout())
                 .phoneNumber(user.getPhoneNumber())
+                .build();
+    }
+
+    public static UserResponse fromUserWithAchievements(User user, List<AchievementsProvider.Achievement> achievements) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .login(user.getLogin())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .profilePhoto(user.getProfilePhoto())
+                .birthday(user.getBirthday())
+                .about(user.getAbout())
+                .phoneNumber(user.getPhoneNumber())
+                .achievements(achievements)
                 .build();
     }
 }

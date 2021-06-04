1a3d56318af5b21e131dcf6a0de525b3c6ef320b
AlexMaz99 <ola_m10@onet.pl>
2021-06-04 15:02:23 +0200

    added photo to peakPost

--- src/main/java/pl/edu/agh/ki/io/api/models/CreatePostRequest.java
+++ src/main/java/pl/edu/agh/ki/io/api/models/CreatePostRequest.java
@@ -2,10 +2,12 @@ package pl.edu.agh.ki.io.api.models;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @NoArgsConstructor
 public class CreatePostRequest {
+    private MultipartFile file;
     private String content;
     private double latitude;
     private double longitude;
--- src/main/java/pl/edu/agh/ki/io/models/wallElements/PeakPost.java
+++ src/main/java/pl/edu/agh/ki/io/models/wallElements/PeakPost.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import pl.edu.agh.ki.io.models.Peak;
 import pl.edu.agh.ki.io.models.User;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -18,6 +19,9 @@ public class PeakPost extends WallItem{
     @ManyToOne
     private Peak peak;
 
+    @Column(name = "photo_path", length = 1023)
+    private String photoPath;
+
     public PeakPost(User user, String content, Peak peak) {
         super(user, content);
         this.peak = peak;
@@ -25,6 +29,18 @@ public class PeakPost extends WallItem{
 
     public PeakPost(User user, String content, Peak peak, double latitude, double longitude) {
         super(user, content, latitude, longitude);
+        this.peak = peak;
+    }
+
+    public PeakPost(User user, String content, Peak peak, String photoPath) {
+        super(user, content);
+        this.peak = peak;
+        this.photoPath = photoPath;
+    }
+
+    public PeakPost(User user, String content, Peak peak, double latitude, double longitude, String photoPath) {
+        super(user, content, latitude, longitude);
+        this.photoPath = photoPath;
         this.peak = peak;
     }
 }

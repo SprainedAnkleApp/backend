093f447790bf20310ae406cc23a2dd3d09b48b90
Dima <zhylko.dima@gmail.com>
2021-06-12 15:57:56 +0200

    buxfix

--- src/main/java/pl/edu/agh/ki/io/db/ChatMessageRepository.java
+++ src/main/java/pl/edu/agh/ki/io/db/ChatMessageRepository.java
@@ -10,15 +10,15 @@ import pl.edu.agh.ki.io.models.ChatMessage;
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
     @Query(value = "select cm from ChatMessage cm " +
-            "where (cm.sender = :senderId and cm.receiver = :receiverId) or " +
-            "      (cm.sender = :receiverId and cm.receiver = :senderId) " +
+            "where (cm.sender.id = :senderId and cm.receiver.id = :receiverId) or " +
+            "      (cm.sender.id = :receiverId and cm.receiver.id = :senderId) " +
             "order by cm.createDate DESC")
     Page<ChatMessage> findChatBySenderIdAndReceiverId(@Param("senderId") Long senderId,
                                                       @Param("receiverId") Long receiverId,
                                                       Pageable pageable);
 
     @Query(value = "select cm from ChatMessage cm " +
-            "where cm.receiver = :receiverId and cm.seen = false " +
+            "where cm.receiver.id = :receiverId and cm.seen = false " +
             "order by cm.createDate DESC")
     Page<ChatMessage> findUnseenByReceiverId(@Param("receiverId") Long receiverId,
                                              Pageable pageable);

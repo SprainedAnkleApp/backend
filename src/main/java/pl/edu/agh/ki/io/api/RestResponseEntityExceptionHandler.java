1b89c66fb453e8e5da2ab87de840caf27306dc96
Dima <zhylko.dima@gmail.com>
2021-06-13 23:36:41 +0200

    exception handling

---
+++ src/main/java/pl/edu/agh/ki/io/api/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,17 @@
+package pl.edu.agh.ki.io.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exception(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.OK);
+    }
+}

b18cb354eaa3119d0d5155c0d39e300e508a5139
Dima <zhylko.dima@gmail.com>
2021-06-14 02:07:40 +0200

    I even don't know how to test it. Probably useless now

--- src/main/java/pl/edu/agh/ki/io/api/RestResponseEntityExceptionHandler.java
+++ src/main/java/pl/edu/agh/ki/io/api/RestResponseEntityExceptionHandler.java
@@ -4,14 +4,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> exception(Exception ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.OK);
+        logger.error(ex.getMessage());
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
